ddcbdf137b3c879d9956b93de273b06a69299ed5
stmayna <stmayna@gmail.com>
2022-11-14 16:23:32 +0700

    working fine

--- estate/models/models.py
+++ estate/models/models.py
@@ -7,12 +7,25 @@ class estate(models.Model):
     _name = 'estate'
     _description = 'estate'
     
-    name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date('Available From',copy=False, default=lambda self: fields.Datetime.today())
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer('Living Area (sqm)')
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    active = fields.Boolean()
+    garden_area = fields.Integer('Garden Area (sqm)')
+    garden_orientation = fields.Selection(
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help="It will help the user to choose between 4 orientation."
+    )
+    state = fields.Selection(
+        selection=[('new', 'New'), ('offer_received,', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        default='new',
+        copy=False,
+    )
