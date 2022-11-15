04d11628fc1cdf40e8791c88bf14a495a492ca87
stmayna <stmayna@gmail.com>
2022-11-15 08:06:29 +0700

    change the file name

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/views_prop.xml',
         #'views/templates.xml',
         'views/menus.xml',
     ],
