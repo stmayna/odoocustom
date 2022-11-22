56c1532cf5a77f5081660a72f69e608ea529f94f
stmayna <stmayna@gmail.com>
2022-11-22 10:33:11 +0700

    Add Property Offer, Chapter 8: Relations Between Models done

--- estate/models/estate_offer.py
+++ estate/models/estate_offer.py
@@ -6,8 +6,10 @@ from odoo import models, fields, api
 class PropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'estate.property.offer'
-    
+
     price = fields.Float()
-    status = fields.Selection(selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
-    partner_id = fields.Many2one('res.partner', required=True )
-    property_id = fields.Many2one('estate', required=True )
+    status = fields.Selection(
+        selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
+    partner_id = fields.Many2one('res.partner', required=True)
+    property_id = fields.Many2one('estate', required=True)
+
--- estate/models/models.py
+++ estate/models/models.py
@@ -3,16 +3,19 @@
 from odoo import models, fields, api
 
 
-class estate(models.Model):
+class Estate(models.Model):
     _name = 'estate'
     _description = 'estate'
-    
+
     name = fields.Char(required=True)
-    user_id = fields.Many2one('res.users', string='Salesperson', index=True, default=lambda self: self.env.user)
-    buyer_id = fields.Many2one('res.partner', string='Buyer', index=True)
+    user_id = fields.Many2one(
+        'res.users', string='Salesperson', index=True, default=lambda self: self.env.user)
+    buyer_id = fields.Many2one(
+        'res.partner', string='Buyer', index=True, copy=False)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date('Available From',copy=False, default=lambda self: fields.Datetime.today())
+    date_availability = fields.Date(
+        'Available From', copy=False, default=lambda self: fields.Datetime.today())
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
@@ -23,14 +26,23 @@ class estate(models.Model):
     active = fields.Boolean()
     garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection(
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        selection=[('north', 'North'), ('south', 'South'),
+                   ('east', 'East'), ('west', 'West')],
         help="It will help the user to choose between 4 orientation."
     )
     state = fields.Selection(
-        selection=[('new', 'New'), ('offer_received,', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        selection=[('new', 'New'), ('offer_received,', 'Offer Received'),
+                   ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
         default='new',
         copy=False,
     )
-    property_type_id = fields.Char(string='Property Type')
-    tag_ids = fields.Char()
-    offer_ids = fields.Many2one('estate.property.offer')
+    property_type_id = fields.Many2one(
+        'estate.property.type', string='Property Type')
+    tag_ids = fields.Many2many('estate.property.tag')
+    offer_ids = fields.One2many(
+        'estate.property.offer', 'property_id', string='Offers')
+
+    # @api.model
+    # def write(self, vals):
+    #     import pdb; pdb.set_trace()
+    #     return super(estate, self).write(vals)
