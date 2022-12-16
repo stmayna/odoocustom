c70470699cdb76ec204e9564e70dfa5478a9815f
stmayna <stmayna@gmail.com>
2022-12-16 07:57:54 +0700

    initiate module for whatsapp integration inherit from res.partner model

---
+++ whatsapp_int/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Whatsapp Integration",
+
+    'summary': """
+        Testing whatsapp integration purpose.
+        """,
+
+    'description': """
+        Development stage.
+    """,
+
+    'author': "Yumi",
+    'website': "https://twitter.com/thepythoncode",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Real Estate/Brokerage',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'estate',
+        'base',
+        'web',
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/whatsapp_int.xml',
+        'views/menus.xml',
+    ],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+    'application': True,
+    'license': "LGPL-3",
+}
---
+++ whatsapp_int/models/whatsapp_int.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, Command
+
+
+class WhatsappInt(models.Model):
+
+    # ---------------------------------------- Private Attributes -------------------------------------
+
+    _inherit = 'res.partner'
+
+    # ---------------------------------------- Fields Declaration -------------------------------------
