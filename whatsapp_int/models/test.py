2689a397da2819dae008c00186d8184404d573d8
stmayna <stmayna@gmail.com>
2022-12-16 10:48:52 +0700

    redirect message done

---
+++ whatsapp_int/models/test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# from heyoo import WhatsApp
+
+
+# def send_message():
+#     messenger = WhatsApp(
+#         'EAALeepQzlZCYBAIZC9b7h8qiK6a0ZBnH9A733b7dZAvwjI9QtTGb162Ll9PJV6z7XacHrZCjeSscEZA7ZABSn6FIdfZCVke42HJTzJcb2q5tJhyfAi9t5G0Gn0Kt3zpnylQHkqktZBxZCSo58F0DSRL4HUMuyZAZCdRma8p2hhnI5XvT2kPZBC9FpY1W1LfOGMJnAT94DSASCN2tVwwZDZD',  phone_number_id='104990505778779')
+#     messenger.send_message('Your message', '6285643497397')
+
+# send_message()
--- whatsapp_int/models/whatsapp_int.py
+++ whatsapp_int/models/whatsapp_int.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, Command
+from odoo import models, fields
+from odoo.exceptions import ValidationError
+from heyoo import WhatsApp
 
 
 class WhatsappInt(models.Model):
@@ -10,3 +12,18 @@ class WhatsappInt(models.Model):
     _inherit = 'res.partner'
 
     # ---------------------------------------- Fields Declaration -------------------------------------
+
+    wa_name = fields.Char()
+    wa_number = fields.Char()
+
+    def send_message(self):
+        if not self.phone:
+            raise ValidationError("Missing phone number.")
+        message = "Hi {name}!".format(name=self.name)
+        wa_redirect_share = 'https://api.whatsapp.com/send?phone={phone}&text={message}'.format(
+            phone=self.phone, message=message)
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'new',
+            'url': wa_redirect_share
+        }
