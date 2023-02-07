4ec3de650671603095807c7b6ab618ff781dac57
stmayna <stmayna@gmail.com>
2023-02-07 16:06:56 +0700

    initiate generate report feature

---
+++ generate_report/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Generate Report",
+
+    'summary': """
+        Testing Inheritance to create report.
+        """,
+
+    'description': """
+        Development stage.
+    """,
+
+    'author': "Yumi",
+    'website': "https://twitter.com/thepythoncode",
+
+    # for the full list
+    'category': 'Mayna Custom',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/whatsapp_int.xml',
+        # 'views/menus.xml',
+    ],
+    'application': True,
+    'license': 'LGPL-3',
+    # 'images': ['statics/whatsapp.png'],
+}
